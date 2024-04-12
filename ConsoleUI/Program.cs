a41081de07e61dfc854790f9e7444cf572d0c017
canyo <canyoldas5410@gmail.com>
2024-04-12 23:16:27 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+//DTO : Data Transformation Object
+//SOLID
+//Open Closed Principle
+
+//ProductManager productManager = new ProductManager(new EfProductDal());
+
+//foreach (var product in productManager.GetByUnitPrice(40, 100))
+//{
+//    Console.WriteLine(product.ProductName);
+//}
+
+//CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+//foreach (var category in categoryManager.GetAll().Data)
+//{
+//    Console.WriteLine(category.CategoryName);
+//}
+
+ProductManager productManager = new ProductManager(new EfProductDal(), new CategoryManager(new EfCategoryDal()));
+
+var result = productManager.GetProductDetails();
+
+if (result.Success)
+{
+    foreach (var product in result.Data)
+    {
+        Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+    }
+}
+else
+{
+    Console.WriteLine(result.Message);
+}
+
